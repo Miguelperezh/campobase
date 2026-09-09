d26deabee2caf1044db6a803b278c055a4e04969
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-09 15:18:43 +0100

    Beta v6: entrada de módulos

---
+++ lab-full/lab/beta-v6.js
@@ -0,0 +1,5 @@
+if(!document.querySelector('#fc-v6-style')){const l=document.createElement('link');l.id='fc-v6-style';l.rel='stylesheet';l.href='lab/beta-v6.css?v=6';document.head.appendChild(l);}
+import './beta-v6-data.js';
+import './beta-v6-shell.js';
+import './beta-v6-training.js';
+import './beta-v6-crud.js';
