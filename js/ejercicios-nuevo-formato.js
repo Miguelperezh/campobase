13d28fd39832aa99d0c4f1b6320fb0972525da18
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-17 14:35:28 +0100

    Integrar 16 ejercicios en F7 y F11 sobre main actual

---
+++ js/ejercicios-nuevo-formato.js
@@ -0,0 +1,19 @@
+import e01 from './ejercicios-nuevo-formato/01-campobase-video-3-finalizaciones-centro-exterior-centro-lateral.js';
+import e02 from './ejercicios-nuevo-formato/02-campobase-video-6-saltos-laterales-knee-drive-sprint-13-7m.js';
+import e03 from './ejercicios-nuevo-formato/03-campobase-video-conduccion-dejar-balon-3-conos-vuelta-lateral-pase.js';
+import e04 from './ejercicios-nuevo-formato/04-campobase-video-conduccion-frenada-planta-sprint-ida-vuelta-recuperacion.js';
+import e05 from './ejercicios-nuevo-formato/05-campobase-video-desplazamiento-lateral-progresivo-pasillo.js';
+import e06 from './ejercicios-nuevo-formato/06-campobase-video-duelos-3v2-finalizacion-robo-rotacion.js';
+import e07 from './ejercicios-nuevo-formato/07-campobase-video-finalizacion-doble-2-balones-transicion-1v1-cambio-carril-v2.js';
+import e08 from './ejercicios-nuevo-formato/08-campobase-pase-balon-espacio-2-conos-1v1-finalizacion-v2.js';
+import e09 from './ejercicios-nuevo-formato/09-campobase-video-pies-rapidos-sprint-3-variaciones.js';
+import e10 from './ejercicios-nuevo-formato/10-campobase-video-reaccion-giro-inicial-senales-laterales-cono-balon-v3.js';
+import e11 from './ejercicios-nuevo-formato/11-campobase-video-reactive-knee-drive-skater-3-variaciones.js';
+import e12 from './ejercicios-nuevo-formato/12-campobase-video-rodillas-altas-laterales-zigzag-cod-sprint.js';
+import e13 from './ejercicios-nuevo-formato/13-campobase-video-conduccion-dejar-balon-3-conos-pase-v2.js';
+import e14 from './ejercicios-nuevo-formato/14-campobase-video-finalizacion-doble-tiro-transicion-1v1-cambio-carril.js';
+import e15 from './ejercicios-nuevo-formato/15-campobase-video-reaccion-espaldas-senales-lateral-giro-cono-balon-v2.js';
+import e16 from './ejercicios-nuevo-formato/16-campobase-video-reaccion-lateral-senal-balon-cono.js';
+
+export const EJERCICIOS_NUEVO_FORMATO = Object.freeze([e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11, e12, e13, e14, e15, e16]);
+export const NUEVOS_EJERCICIOS_IDS = Object.freeze(EJERCICIOS_NUEVO_FORMATO.map(({ id }) => id));
