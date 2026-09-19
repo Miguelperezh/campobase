ba7f8955cbe1c9e3c9b42166172a4ac48a08a531
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-19 14:54:23 +0100

    Hotfix: forzar PWA actual y evitar bundle antiguo
    * fix: forzar arranque PWA con build actual
    
    * fix: registrar service worker del build actual
    
    * fix: forzar módulo de acceso SaaS actual
    
    * fix: renovar cache PWA y forzar HTML desde red
    
    * test: cubrir build actual y evitar bundle antiguo
    
    * test: actualizar build PWA actual
    
    * test: actualizar build PWA actual
    
    * test: actualizar build PWA actual
    
    * test: actualizar build PWA actual
    
    * docs: registrar hotfix crítico de caché PWA

--- js/app.js
+++ js/app.js
@@ -5721,7 +5721,7 @@ async function init() {
       }
       if (!wasControlled) sessionStorage.removeItem(reloadKey);
     } else {
-      navigator.serviceWorker.register('./sw.js?v=20260919-refreshfix-v5').then((reg) => {
+      navigator.serviceWorker.register('./sw.js?v=20260919-prod-current-v6').then((reg) => {
         reg.update().catch(() => {});
       }).catch(handleError);
       navigator.serviceWorker.addEventListener('controllerchange', () => {
