f092db678a8e8306dbf7d5365d786592df023f86
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-22 14:08:18 +0100

    fix(pwa): regenerar caché móvil v33 sin alterar datos

--- tests/pwa-current-build.test.js
+++ tests/pwa-current-build.test.js
@@ -9,7 +9,7 @@ const [html, app, sw, cloud] = await Promise.all([
   readFile(new URL('../js/supabase-client.js', import.meta.url), 'utf8'),
 ]);
 
-const BUILD = '20260921-cloud-recovery-v32';
+const BUILD = '20260922-mobile-video-cache-v33';
 
 test('index app cloud y service worker apuntan al mismo build actual', () => {
   assert.match(html, new RegExp(BUILD));
