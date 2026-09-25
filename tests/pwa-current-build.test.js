83f783a280496382ee8d4ef74fca382ff0f0d954
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-25 21:42:35 +0100

    test: alinear build esperado v62

--- tests/pwa-current-build.test.js
+++ tests/pwa-current-build.test.js
@@ -9,7 +9,7 @@ const [html, app, sw, cloud] = await Promise.all([
   readFile(new URL('../js/supabase-client.js', import.meta.url), 'utf8'),
 ]);
 
-const BUILD = '20260924-v59-delegate-views-visible-render-fix';
+const BUILD = '20260925-v62-equipo-prep-rootcause';
 
 test('index app cloud y service worker apuntan al mismo build actual', () => {
   assert.match(html, new RegExp(BUILD));
