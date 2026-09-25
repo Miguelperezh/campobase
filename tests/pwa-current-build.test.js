c17ab68d8aa636e45c87aa4855d276c66dd26d84
Miguelperezh <mpestrategiaacademy@gmail.com>
2026-09-25 20:14:30 +0100

    Align tests/pwa-current-build.test.js to v61 navigation build

--- tests/pwa-current-build.test.js
+++ tests/pwa-current-build.test.js
@@ -9,7 +9,7 @@ const [html, app, sw, cloud] = await Promise.all([
   readFile(new URL('../js/supabase-client.js', import.meta.url), 'utf8'),
 ]);
 
-const BUILD = '20260925-v60-equipo-prep-persistence';
+const BUILD = '20260925-v61-equipo-nav-persistence';
 
 test('index app cloud y service worker apuntan al mismo build actual', () => {
   assert.match(html, new RegExp(BUILD));
